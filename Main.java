b20e34f768774dfb2bf8fd3aee06d297d990d9c9
jh0nbyron <49107469+jhonbyronquirozperez@users.noreply.github.com>
2021-09-02 00:45:47 +0000

    Ejercicio 1 Taller 2

--- Main.java
+++ Main.java
@@ -3,54 +3,33 @@ import java.util.Scanner;
 
 
 class Main {
-  static Scanner sc= new Scanner(System.in);
+  public static void main(String[] args){
+    //primer numero
+    Scanner entrada = new Scanner(System.in);
+    int numero1;
 
-  public static void main(String[] args) {
-
-    Scanner sc= new Scanner(System.in);
-
-    int edad;
-            double estatura=0;
-            double media_edad=0;
-            double media_altura=0;
-            double suma_edad=0;
-            double suma_alt=0;
-            int cont_edad=0;
-            int cont_alt=0;
-           
-            for(int i=0;i<=4;i++)
-            {
-                System.out.println("Introduce edad");
-                edad=sc.nextInt();
-               
-                System.out.println("Introduce altura");
-                estatura=sc.nextInt();
-               
-                if(edad>18)
-                {
-                    cont_edad++;
-                    suma_edad=suma_edad+edad;   
-                }
-                if(estatura>(1.75))
-                {
-                    cont_alt++;
-                    suma_alt=suma_alt+estatura;   
-                }
-               
-            }//fin de for
-           
-            media_edad=suma_edad/cont_edad;
-            media_altura=suma_alt/cont_alt;
-           
-            System.out.println("Edad media: "+media_edad);
-            System.out.println("Altura media: "+media_altura);
-            System.out.println("Alumnos mayores de 18 años: "+cont_edad);
-            System.out.println("Alumnos con altura mas de 1.75:" +cont_alt);
+    System.out.println("ingrese el primer numero: ");
+    numero1 = entrada.nextInt();
 
 
+    //segundo numero
+    Scanner entrada2 = new Scanner(System.in);
+    int numero2;
+
+    System.out.println("ingrese el segundo numero: ");
+    numero2 = entrada2.nextInt();
+   
+
+   System.out.println(numero1);
+   System.out.println(numero2);
+
+   if(numero1 == numero2){
+     System.out.println("los dos numeros son iguales");
+   } else {
+     System.out.println("los dos numeros son diferentes");
+   }
 
 
-    
 
   }
 }
